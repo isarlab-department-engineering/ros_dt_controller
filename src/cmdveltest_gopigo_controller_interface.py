018c3f1a9c5f410d8adaaced1e1cd5bf8a126a85
Alberto <alberto.dionigi95@gmail.com>
2019-10-08 17:50:26 +0200

    Aggiunto nuovo messaggio, aggiunta interfaccia gopigo per andare dritto di tot cm e girare di tot gradi

--- src/cmdveltest_gopigo_controller_interface.py
+++ src/cmdveltest_gopigo_controller_interface.py
@@ -5,54 +5,69 @@ import tf.transformations
 import easygopigo3 as easy
 import numpy as np
 from geometry_msgs.msg import Twist
+from controller.msg import *
 
 class motor_driver: 
-	
-	# const
-	WHEEL_RADIUS = 0.03325 # radius of wheels
-	WHEEL_DISTANCE = 0.117 # distance between wheels
-	# global var
-	rightSpeed = 0	
-	leftSpeed = 0
-	
-	def atExitFunction(self):
-	        self.turnOffMotors()
+    
+    # const
+    WHEEL_RADIUS = 0.03325 # radius of wheels
+    WHEEL_DISTANCE = 0.117 # distance between wheels
+    # global var
+    rightSpeed = 0  
+    leftSpeed = 0
+    
+    def atExitFunction(self):
+            self.turnOffMotors()
 
-	def turnOffMotors(self):
-		# log stop info
-		rospy.loginfo("Turn off motors Turn on stop lights")
-		# turn off motors
-		self.gpg.stop()		
+    def turnOffMotors(self):
+        # log stop info
+        rospy.loginfo("Turn off motors Turn on stop lights")
+        # turn off motors
+        self.gpg.stop()     
 
-	def callback(self,data):
-		rospy.loginfo("Received a /cmd_vel message!")
-		rospy.loginfo("Linear Components: [%f, %f, %f]"%(data.linear.x, data.linear.y, data.linear.z))
-		rospy.loginfo("Angular Components: [%f, %f, %f]"%(data.angular.x, data.angular.y, data.angular.z))
-		# Use the kinematics of your robot to map linear and angular velocities into motor commands
-		vLeft = data.linear.x - (data.angular.z * self.WHEEL_DISTANCE / 2)
-		vRight = data.linear.x + (data.angular.z * self.WHEEL_DISTANCE / 2)
-		self.leftSpeed = 360* vLeft / (2 * np.pi * self.WHEEL_RADIUS)
-    		self.rightSpeed = 360 * vRight / (2 * np.pi * self.WHEEL_RADIUS)
-		rospy.loginfo("Left,Right speed: [%d, %d]"%(self.leftSpeed, self.rightSpeed))
-		self.gpg.set_motor_dps(self.gpg.MOTOR_LEFT,int(self.leftSpeed))
-		self.gpg.set_motor_dps(self.gpg.MOTOR_RIGHT,int(self.rightSpeed))    
+    def callback(self,data):
+        rospy.loginfo("Received a /cmd_vel message!")
+        rospy.loginfo("Linear Components: [%f, %f, %f]"%(data.linear.x, data.linear.y, data.linear.z))
+        rospy.loginfo("Angular Components: [%f, %f, %f]"%(data.angular.x, data.angular.y, data.angular.z))
+        # Use the kinematics of your robot to map linear and angular velocities into motor commands
+        vLeft = data.linear.x - (data.angular.z * self.WHEEL_DISTANCE / 2)
+        vRight = data.linear.x + (data.angular.z * self.WHEEL_DISTANCE / 2)
+        self.leftSpeed = 360* vLeft / (2 * np.pi * self.WHEEL_RADIUS)
+            self.rightSpeed = 360 * vRight / (2 * np.pi * self.WHEEL_RADIUS)
+        rospy.loginfo("Left,Right speed: [%d, %d]"%(self.leftSpeed, self.rightSpeed))
+        self.gpg.set_motor_dps(self.gpg.MOTOR_LEFT,int(self.leftSpeed))
+        self.gpg.set_motor_dps(self.gpg.MOTOR_RIGHT,int(self.rightSpeed))  
 
-	def __init__(self):
-		# motor setup
-		self.gpg = easy.EasyGoPiGo3() # GoPiGo3 Motor
-		rospy.Subscriber("cmd_vel", Twist, self.callback, queue_size=1) # subscribe to cmd_vel topic
-		rospy.loginfo("Initialized controller class")
+    def callback_gpg(self,data):
+        rospy.loginfo("Received a /gopigo_vel message!")
+        mode = data.mode
+        move = data.move
+
+        if(mode=="cm"):
+            self.gpg.drive_cm(move)
+        elif(mode=="deg"):
+            self.gpg.drive_degrees(move)
+        else:
+            rospy.loginfo("Messaggio in un formato non riconosciuto")
+
+    def __init__(self):
+        # motor setup
+        self.gpg = easy.EasyGoPiGo3() # GoPiGo3 Motor
+        self.gpg.set_robot_constants(WHEEL_RADIUS*2*1000, WHEEL_DISTANCE*1000)
+        rospy.Subscriber("cmd_vel", Twist, self.callback, queue_size=1) # subscribe to cmd_vel topic
+        rospy.Subscriber("gopigo_vel", Gopigo, self.callback_gpg, queue_size=1) # subscribe to cmd_vel topic
+        rospy.loginfo("Initialized controller class")
 
 def main(args):
     m_driver = motor_driver()
     @atexit.register
     def atExitClass():
-    	m_driver.atExitFunction()
+        m_driver.atExitFunction()
     rospy.init_node('base_controller')
     try:
-	    rospy.spin() # loop until shutdown
+        rospy.spin() # loop until shutdown
     except KeyboardInterrupt:
-	    print("Shutting down")
+        print("Shutting down")
 
 if __name__ == '__main__':
     main(sys.argv)
